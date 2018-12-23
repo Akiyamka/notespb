9fdee744f2d9ba6bdc33d8ac6c393f13e49b059d
Alexander Dubinin <akiyamka@gmail.com>
2018-12-24 01:15:36 +0300

    add social btns

--- scripts/smooth_link.js
+++ scripts/smooth_link.js
@@ -16,23 +16,23 @@ $(document).ready(function(){
 	});
 });
 
-$(document).ready(function(){
-	$("#goup").on("click","a", function (event) {
-		event.preventDefault();
-		var id  = $(this).attr('href'),
-			top = $(id).offset().top;
-		$('body,html').animate({scrollTop: top}, 1000);
-	});
-});
-// скрипт скрытия кнопки вверх
+// $(document).ready(function(){
+// 	$("#goup").on("click","a", function (event) {
+// 		event.preventDefault();
+// 		var id  = $(this).attr('href'),
+// 			top = $(id).offset().top;
+// 		$('body,html').animate({scrollTop: top}, 1000);
+// 	});
+// });
+// // скрипт скрытия кнопки вверх
 
-window.onload = function() {
-	var scrollUp = document.getElementById('goup'); // найти элемент
-	window.onscroll = function () {
-		if ( window.pageYOffset > 0 ) {
-			goup.style.display = 'block';
-		} else {
-			goup.style.display = 'none';
-		}
-	};
-};
+// window.onload = function() {
+// 	var scrollUp = document.getElementById('goup'); // найти элемент
+// 	window.onscroll = function () {
+// 		if ( window.pageYOffset > 0 ) {
+// 			goup.style.display = 'block';
+// 		} else {
+// 			goup.style.display = 'none';
+// 		}
+// 	};
+// };
